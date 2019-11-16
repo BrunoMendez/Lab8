93ea1cad739ccce204016468ad6ac6f043623be1
BrunoMendez <bruno.m.275@gmail.com>
2019-11-16 15:59:38 -0700

    update

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://bruno:Kal-el97@cluster0-iidfe.mongodb.net/bruno-blog-posts?retryWrites=true&w=majority";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://public:1234@cluster0-iidfe.mongodb.net/bruno-blog-posts?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
